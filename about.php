e47300ebb0af3ac0bcb3ceb4d82ee19b703288ae
Yusup Maulana <yusupmaulana950@gmail.com>
2017-11-06 14:34:01 +0700

    halaman tentang website

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'core/init.php';
+require_once 'view/header.php';
+?>
+
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col m12"><br>
+      <h5>Tentang Dprint</h5>
+      <p class="light justify">Ide awal berdirinya Dprint adalah untuk memudahkan seseorang untuk menemukan percetakan yang berada disekitar(khususnya Kota Depok). Adakalanya seseorang lupa dimana tempat yang dia butuhkan untuk mencetak laporan, jurnal, makalah dan lain-lain. Itu dikarenakan sifat alami dari manusia dan semoga dengan adanya Dprint ini bisa membantu mencari percetakan yang dimaksud.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col m12">
+        <h5>Kontak</h5>
+        <p class="light">Jln. Margonda Raya, Gang kapuk, Jln Bungur 2, rt 01/ rw 02 no. 38<br>
+        Pondok Cina, Beji, Depok<br>
+        Jawa Barat 16424<br>
+        (123) 456-7890<br>
+        <a href="mailto:#">admin@dprint.co.id</a>
+        </p>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+<?php require_once 'view/footer.php' ?>
